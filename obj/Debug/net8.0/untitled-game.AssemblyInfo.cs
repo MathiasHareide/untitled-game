235295e193d7bd9494d2216bd0c819a58a4ba718
Mathias <mathias.hareide4497@gmail.com>
2024-12-30 10:18:10 +0100

    rename GameObject.cs to IGameObject

--- obj/Debug/net8.0/untitled-game.AssemblyInfo.cs
+++ obj/Debug/net8.0/untitled-game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("untitled-game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6946355f62a9bf0f1d6be9827bfa11d7cdadebaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d068921160c539aeeb346f5bab83aed7a557b6a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("untitled-game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("untitled-game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
