84a80304fa6437f9c852fb3eaef06294e844ccd9
Mathias <mathias.hareide4497@gmail.com>
2025-05-08 08:50:35 +0200

    Add test gameobjects

--- scripts/IGameObject.cs
+++ scripts/IGameObject.cs
@@ -1,48 +1,153 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using lamo;
 
 public interface IGameObject
 {
-    Vector2 Position { get; set; }
+    Vector2 Position { get; }
+    Texture2D Sprite { get; }
+    int SpriteHeight { get; }
+    int SpriteWidth { get; }
 
     void Update(GameTime gameTime);
     void Draw(SpriteBatch spriteBatch);
 }
 
-public class TestGameObject : IGameObject
+public class TestGameObject(Vector2 position) : IGameObject
 {
-    private Texture2D _texture;
+    public Texture2D Sprite { get; private set; }
+    public Vector2 Position { get; private set; } = position;
+    public int SpriteHeight { get; private set; } = 50;
+    public int SpriteWidth { get; private set;} = 50;
 
-    public Vector2 Position { get; set; }
-
-    public TestGameObject(Texture2D texture)
-    {
-        _texture = texture;
-    }
+    private Vector2 _spriteScale;
 
     public void Update(GameTime gameTime)
     {
+        var dir = new Vector2(0, 0);
         if (Keyboard.GetState().IsKeyDown(Keys.W))
-        {   
-            Position = new Vector2(Position.X, Position.Y - 5f);
+        {
+            dir.Y -= 1;
         }
         else if (Keyboard.GetState().IsKeyDown(Keys.S))
-        {   
-            Position = new Vector2(Position.X, Position.Y + 5f);
+        {
+            dir.Y += 1;
         }
         if (Keyboard.GetState().IsKeyDown(Keys.A))
-        {   
-            Position = new Vector2(Position.X - 5f, Position.Y);
+        {
+            dir.X -= 1;
         }
         else if (Keyboard.GetState().IsKeyDown(Keys.D))
         {
-            Position = new Vector2(Position.X + 5f, Position.Y);
+            dir.X += 1;
+        }
+        if (dir != Vector2.Zero)
+        {
+            dir.Normalize();
+        }
+        Position += dir * 5f;
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(Sprite, Position, null, Color.White, 0, Vector2.Zero, _spriteScale, SpriteEffects.None, 0);
+    }
+
+    public void SetSprite(Texture2D sprite)
+    {
+        Sprite = sprite;
+        _spriteScale = new(SpriteWidth / Sprite.Width, SpriteHeight / Sprite.Height);
+    }
+}
+
+public class ClickableGameObject(Vector2 position) : IGameObject
+{
+    public Texture2D Sprite { get; private set; }
+    public Vector2 Position { get; private set; } = position;
+    public int SpriteHeight { get; private set; } = 35;
+    public int SpriteWidth { get; private set;} = 35;
+
+    private Vector2 _spriteScale;
+
+    public void Update(GameTime gameTime)
+    {
+        var mouseState = Mouse.GetState();
+        if (MouseIsHovering(mouseState) && MouseStateManager.Instance.StartedPressingLMBThisFrame())
+        {
+            Console.WriteLine("ClickableGameObject has been clicked on.");
         }
     }
 
     public void Draw(SpriteBatch spriteBatch)
     {
-        spriteBatch.Draw(_texture, Position, null, Color.White, 0, Vector2.Zero, 50, SpriteEffects.None, 0);
+        spriteBatch.Draw(Sprite, Position, null, Color.White, 0, Vector2.Zero, _spriteScale, SpriteEffects.None, 0);
+    }
+
+    public void SetSprite(Texture2D sprite)
+    {
+        Sprite = sprite;
+        _spriteScale = new(SpriteWidth / Sprite.Width, SpriteHeight / Sprite.Height);
+    }
+
+    private bool MouseIsHovering(MouseState mouseState)
+    {
+        if (mouseState.Position.X >= Position.X && mouseState.Position.X <= Position.X + _spriteScale.X)
+        {
+            if (mouseState.Position.Y >= Position.Y && mouseState.Position.Y <= Position.Y + _spriteScale.Y)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+public class DragableGameObject(Vector2 position) : IGameObject
+{
+    public Texture2D Sprite { get; private set; }
+    public Vector2 Position { get; private set; } = position;
+    public int SpriteHeight { get; private set; } = 50;
+    public int SpriteWidth { get; private set;} = 80;
+
+    private Vector2 _spriteScale;
+    private Vector2 _mousePositionLastFrame;
+    private bool _shouldFollowMousePosition = false;
+
+    public void Update(GameTime gameTime)
+    {
+        var mouseState = Mouse.GetState();
+        if (ShouldStartFollowingMousePosition())
+        {
+            _shouldFollowMousePosition = true;
+        }
+        else if (mouseState.LeftButton == ButtonState.Released)
+        {
+            _shouldFollowMousePosition = false;
+        }
+        if (_shouldFollowMousePosition)
+        {
+            Position += mouseState.Position.ToVector2() - _mousePositionLastFrame;
+        }
+        _mousePositionLastFrame = mouseState.Position.ToVector2();
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(Sprite, Position, null, Color.White, 0, Vector2.Zero, _spriteScale, SpriteEffects.None, 0);
+    }
+
+    public void SetSprite(Texture2D sprite)
+    {
+        Sprite = sprite;
+        _spriteScale = new(SpriteWidth / Sprite.Width, SpriteHeight / Sprite.Height);
+    }
+
+    private bool ShouldStartFollowingMousePosition()
+    {
+        return MouseStateManager.Instance.IsMouseOverGameObject(this) && MouseStateManager.Instance.StartedPressingLMBThisFrame();
     }
 }
--- scripts/Main.cs
+++ scripts/Main.cs
@@ -1,3 +1,4 @@
+﻿using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -8,27 +9,40 @@ public class Main : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+
     private TestGameObject _test;
+    private ClickableGameObject _clickable;
+    private DragableGameObject _dragable;
 
     public Main()
     {
-        _graphics = new GraphicsDeviceManager(this);
+        _graphics = new GraphicsDeviceManager(this)
+        {
+            PreferredBackBufferWidth = 1080,
+            PreferredBackBufferHeight = 720
+        };
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
-        Window.AllowUserResizing = true;
+        _graphics.ApplyChanges();
     }
 
     protected override void Initialize()
     {
+        _test = new TestGameObject(new (50, 50));
+        _clickable = new ClickableGameObject(new (250, 50));
+        _dragable = new DragableGameObject(new (450, 50));
+        SceneManager.Instance.AddGameObject(_test);
+        SceneManager.Instance.AddGameObject(_clickable);
+        SceneManager.Instance.AddGameObject(_dragable);
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-        Texture2D whitePixel = Content.Load<Texture2D>("whitePixel");
-        _test = new TestGameObject(whitePixel);
-        SceneManager.Instance.AddGameObject(_test);
+        _test.SetSprite(Content.Load<Texture2D>("whitepixel"));
+        _clickable.SetSprite(Content.Load<Texture2D>("whitepixel"));
+        _dragable.SetSprite(Content.Load<Texture2D>("whitepixel"));
     }
 
     protected override void Update(GameTime gameTime)
@@ -44,8 +58,8 @@ public class Main : Game
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
-        _spriteBatch.Begin();
+        GraphicsDevice.Clear(Color.DimGray);
+        _spriteBatch.Begin(SpriteSortMode.BackToFront);
         SceneManager.Instance.Draw(_spriteBatch);
         _spriteBatch.End();
         base.Draw(gameTime);
